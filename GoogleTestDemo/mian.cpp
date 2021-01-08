7b99ec136ad57f859c10954c470b3e43a984881a
DH <53322041+D-Hon@users.noreply.github.com>
2021-01-08 16:50:05 +0800

    补充自定义显示错误信息

--- GoogleTestDemo/mian.cpp
+++ GoogleTestDemo/mian.cpp
@@ -15,7 +15,6 @@ bool Even(int nValue)
 	return (0 == (nValue % 2)) ? true : false;
 }
 
-//////////////////////////////////////////////////////////////////////////
 TEST(GT, Odd)
 {
 	for (int i = 0; i < 4; i++)
@@ -34,6 +33,33 @@ TEST(GT, Even)
 		{
 			EXPECT_EQ(true, Even(i)) << i << " 不为偶数";
 		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+//自定义打印信息
+//funtion(const char* expr_1, int nValue1)    EXPECT_PRED_FORMAT1(funtion, nValue1);
+//funtion(const char* expr_1, int nValue1, const char* expr_2, int nValue2)    EXPECT_PRED_FORMAT2(funtion, nValue1, nValue2);
+//EXPECT_PRED_FORMAT1,..EXPECT_PRED_FORMAT5
+testing::AssertionResult OddF(const char* m_expr, int nValue)
+{
+	if (1 == (nValue % 2))
+	{
+		return testing::AssertionSuccess();
+	}
+	else
+	{
+		testing::Message msg;
+		msg << "参数 " << m_expr << " = " << nValue << " 不是奇数";
+		return testing::AssertionFailure(msg);
+	}
+}
+
+TEST(GT, OddF)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		EXPECT_PRED_FORMAT1(OddF, i);
 	}
 }
 
@@ -62,8 +88,8 @@ TEST_F(CGT, CEven)
 int main(int argc, char* argv[])
 {
 	//把事件挂卡来，先挂起的包住后挂起的。
-	testing::AddGlobalTestEnvironment(new FooEnvironment_First);
-	testing::AddGlobalTestEnvironment(new FooEnvironment_Second);
+	//testing::AddGlobalTestEnvironment(new FooEnvironment_First);
+	//testing::AddGlobalTestEnvironment(new FooEnvironment_Second);
 
 	testing::InitGoogleTest(&argc, argv);
 
